f463ae29404544d5e5cf5bb27e29a9a90e137113
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-30 19:03:47 -0300

    task 6 Fix output

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,13 +40,13 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def numberinhtml(n):
     """the number in html"""
-    return render_template("5-number.html", n=int(n))
+    return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def numberinhtmlevenold(n):
     """the number in html"""
-    return render_template('6-number_odd_or_even.html', n=int(n))
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == "__main__":
