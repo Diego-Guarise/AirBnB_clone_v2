58afcb715f2fe31a1ab526f9fe34d112a9eddbd9
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-30 18:47:35 -0300

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """print hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """print hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def Cis(text):
+    """print C is string"""
+    return "C %s" % escape(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
